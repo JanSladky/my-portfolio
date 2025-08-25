60a8e51011f260a0b8d92558c1a319d2a486f23e
Jan Sladký <sladky.honza@gmail.com>
2025-08-25 14:20:48 +0200

    fix ukládání obrázků

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -1,1 +1,11 @@
-export default () => ({});
+// backend/config/plugins.ts
+export default ({ env }) => ({
+  upload: {
+    config: {
+      provider: 'local',
+      providerOptions: {
+        sizeLimit: 10000000, // 10 MB (nebo podle potřeby)
+      },
+    },
+  },
+});
