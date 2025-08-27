eaf29c956755314ba1ce4f030096a7baec32918b
Jan Sladký <sladky.honza@gmail.com>
2025-08-27 16:22:50 +0200

    Fix admin přihlášení

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -1,8 +1,32 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:', 'http:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https:', 'http:'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https:', 'http:'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: [
+        'https://jansladky.eu',          // tvoje hlavní doména
+        'https://www.jansladky.eu',      // pokud používáš i www
+        'https://admin.jansladky.eu',    // pokud uděláš subdoménu
+        'https://strapi-production-d581.up.railway.app', // fallback
+      ],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
